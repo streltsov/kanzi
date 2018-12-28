b053c7d46960658d85b97d348526e9d1919b9c8f
w3punk <w3punk@pm.me>
2018-12-28 14:06:01 +0300

    Apply inView.js

--- content_scripts/kanzi.js
+++ content_scripts/kanzi.js
@@ -1,8 +1,28 @@
+let tags = ['p'];
 browser.storage.local.get().then((dict) => {
-  for (let word in dict) {
-    wrapWord(word, dict[word].meaning, dict[word].example);
-    createTooltip(word, dict[word].meaning, dict[word].example);
-  }
+
+  tags.forEach(tag => {
+
+    inView(tag).on('enter', tag => {
+
+      if (!tag.classList.contains('kz-done')) {
+
+        let t0 = performance.now();
+
+        Object.keys(dict).forEach(word => {
+          let re = new RegExp('(\\b' + word + '\\b|\\b' + word + '{0,2}(i?ed|i?e?s|ing|er|or|i?ly|ication|ion|ness)\\b)' + '(?![^<]*>|[^<>]*<\/)', 'gi');
+          tag.innerHTML = tag.innerHTML.replace(re, `<span class="kz-word kz-${word.replace(/\s/g, '_')}">$&</span>`);
+          createTooltip(word, dict[word].meaning, dict[word].example);
+        });
+
+        tag.classList.add('kz-done');
+
+        let t1 = performance.now();
+        console.log('Tag ' + '<' + tag.tagName + '> took: ' + (t1 - t0) + ' milliseconds');
+
+      } // if contains
+    }); // inView
+  }); // forEach tag in array
 });
 
 browser.runtime.onMessage.addListener(request => createModal(request.selectedText));
