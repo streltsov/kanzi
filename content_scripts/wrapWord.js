a1bba79bfcb466888b7d65344797149cac61a323
w3punk <w3punk@pm.me>
2019-01-06 20:19:45 +0300

    Add ability to add phrases

--- content_scripts/wrapWord.js
+++ content_scripts/wrapWord.js
@@ -1,31 +1,34 @@
 function wrapWord(word) {
+
+  let suffixes = '{0,2}(i?ed|i?e?s|ing|er|or|y|i?ly|ally|able|ication|a?t?ion|ness|)';
+
+  search = word.split(' ');
+  search.forEach(function(value, index, array) {
+    array[index] = value + suffixes;
+  });
+  search = search.join(' ');
+
   let nodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-  let re = new RegExp('(\\b' + word + '\\b|\\b' + word + '{0,2}(i?ed|i?e?s|ing|er|or|y|i?ly|ally|able|ication|a?t?ion|ness)\\b)', 'gi');
+  let re = new RegExp('(\\b' + search + '\\b)', 'gi');
+
+  let wordAmount = word.split(' ').length;
 
   while (nodes.nextNode()) {
 
     if (nodes.currentNode.textContent.trim()) {
 
-      if (nodes.currentNode.textContent.search(re) != -1) {
-
+      while (nodes.currentNode.textContent.search(re) != -1) {
         let wordStart = nodes.currentNode.textContent.search(re);
-        let wordLength;
-
-        for (i = wordStart + 1; /\w/.test(nodes.currentNode.textContent.charAt(i)); i++) {
-          wordLength = (i + 1) - wordStart;
-        }
-
         let splitNode = nodes.currentNode.splitText(wordStart);
         nodes.nextNode();
-        let wordInText = nodes.currentNode.data.slice(0, wordLength);
-        nodes.currentNode.data = nodes.currentNode.data.slice(wordLength)
+        let wordInText = nodes.currentNode.data.split(' ', wordAmount).join(' ').trim();
+        nodes.currentNode.data = nodes.currentNode.data.slice(wordInText.length);
         let span = document.createElement('span');
         span.appendChild(document.createTextNode(wordInText));
         nodes.currentNode.parentElement.insertBefore(span, splitNode);
-        span.className = `kz-word kz-${word}`
+        span.className = `kz-word kz-${word.replace(/\s/g, '_')}`;
 
       }
     }
   }
 }
-
