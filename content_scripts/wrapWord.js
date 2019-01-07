1d847e3d4bb0e0225a3d30fc8fa4df52e9a4908d
w3punk <w3punk@pm.me>
2019-01-07 17:47:10 +0300

    Fix bug with non-alphanumeric characters
    Fix bug with non-alphanumeric characters after string, they were wrapped along with a string.

--- content_scripts/wrapWord.js
+++ content_scripts/wrapWord.js
@@ -1,17 +1,16 @@
-function wrapWord(word) {
+function wrapWord(string) {
 
   let suffixes = '{0,2}(i?ed|i?e?s|ing|er|or|y|i?ly|ally|able|ication|a?t?ion|ness|)';
 
-  search = word.split(' ');
-  search.forEach(function(value, index, array) {
+  let regexPattern = string.split(' ');
+  regexPattern.forEach(function(value, index, array) {
     array[index] = value + suffixes;
   });
-  search = search.join(' ');
+  regexPattern = '(\\b' + regexPattern.join(' ') + '\\b)';
+  let re = new RegExp(regexPattern, 'gi');
 
   let nodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-  let re = new RegExp('(\\b' + search + '\\b)', 'gi');
-
-  let wordAmount = word.split(' ').length;
+  let wordAmount = string.split(' ').length;
 
   while (nodes.nextNode()) {
 
@@ -21,12 +20,19 @@ function wrapWord(word) {
         let wordStart = nodes.currentNode.textContent.search(re);
         let splitNode = nodes.currentNode.splitText(wordStart);
         nodes.nextNode();
-        let wordInText = nodes.currentNode.data.split(' ', wordAmount).join(' ').trim();
-        nodes.currentNode.data = nodes.currentNode.data.slice(wordInText.length);
+
+        let stringOnPage = nodes.currentNode.data.split(' ', wordAmount);
+
+	if (/\W|_/.test(stringOnPage[wordAmount - 1])) {
+	  stringOnPage[wordAmount - 1] = stringOnPage[wordAmount - 1].replace(/\W.*/, '');
+	  }
+        
+	stringOnPage = stringOnPage.join(' ');
+        nodes.currentNode.data = nodes.currentNode.data.slice(stringOnPage.length);
         let span = document.createElement('span');
-        span.appendChild(document.createTextNode(wordInText));
+        span.appendChild(document.createTextNode(stringOnPage));
         nodes.currentNode.parentElement.insertBefore(span, splitNode);
-        span.className = `kz-word kz-${word.replace(/\s/g, '_')}`;
+        span.className = `kz-word kz-${string.replace(/\s/g, '_')}`;
 
       }
     }
