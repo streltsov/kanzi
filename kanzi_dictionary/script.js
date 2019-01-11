d17ebbeee52449ab7cc28c31ac36744ec2042f17
w3punk <w3punk@pm.me>
2019-01-11 16:32:31 +0300

    Add Export/Import dict as JSON feature

--- kanzi_dictionary/script.js
+++ kanzi_dictionary/script.js
@@ -24,3 +24,47 @@ browser.storage.local.get().then((dict) => {
     document.getElementsByTagName('table')[0].appendChild(row);
   });
 });
+
+document.getElementsByClassName("export-button")[0].addEventListener("click", function() {
+  browser.storage.local.get().then((dict) => {
+    downloadObjectAsJson(dict, "Kanzi Dictionary")
+  })
+})
+
+function downloadObjectAsJson(exportObj, exportName) {
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+  const fileList = this.files;
+  printFile(fileList[0]);
+}
+
+function printFile(file) {
+  var reader = new FileReader();
+  reader.onload = function(evt) {
+    browser.storage.local.get().then((dict) => {
+      let iDict = JSON.parse(evt.target.result);
+      Object.keys(iDict).forEach(word => {
+        if (!dict.hasOwnProperty(word)) {
+          browser.storage.local.set({
+            [word]: {
+              meaning: iDict[word].meaning,
+              example: iDict[word].example
+            }
+          });
+        }
+      })
+    })
+  };
+  reader.readAsText(file);
+}
