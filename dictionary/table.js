f00067890b54e3c724ab99f2bb60a3c4d094d081
w3punk <w3punk@pm.me>
2019-01-12 16:58:40 +0300

    Add cell action onblur

--- dictionary/table.js
+++ dictionary/table.js
@@ -5,21 +5,31 @@ browser.storage.local.get().then((dict) => {
   Object.keys(dict).sort().forEach(word => {
 
     let row = document.createElement('tr');
+    row.id = word.trim().replace(/\s/g, '_');
 
     let wordColumn = document.createElement('td');
-    wordColumn.contentEditable = "true"; 
+    wordColumn.contentEditable = "true";
+    wordColumn.onblur = function() {
+
+      let oldWord = wordColumn.parentNode.id.replace(/_/g, ' ');
+      let newWord = wordColumn.innerText.trim().toLowerCase();
+
+      console.log(oldWord);
+      console.log(newWord);
+
+    };
     let wordColumnText = document.createTextNode(word);
     wordColumn.appendChild(wordColumnText);
     row.appendChild(wordColumn);
 
     let meaningColumn = document.createElement('td');
-    meaningColumn.contentEditable = "true"; 
+    meaningColumn.contentEditable = "true";
     let meaningColumnText = document.createTextNode(dict[word].meaning);
     meaningColumn.appendChild(meaningColumnText);
     row.appendChild(meaningColumn);
 
     let exampleColumn = document.createElement('td');
-    exampleColumn.contentEditable = "true"; 
+    exampleColumn.contentEditable = "true";
     let exampleColumnText = document.createTextNode(dict[word].example);
     exampleColumn.appendChild(exampleColumnText);
     row.appendChild(exampleColumn);
