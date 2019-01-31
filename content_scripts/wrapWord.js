f5bc7c44b7ab5d6523a60cdb3bbf1b05334dcb56
w3punk <w3punk@pm.me>
2019-01-31 13:52:22 +0300

    Apply display:inline to words

--- content_scripts/wrapWord.js
+++ content_scripts/wrapWord.js
@@ -30,7 +30,7 @@ function wrapWord(string) {
     span.appendChild(document.createTextNode(stringOnPage));
     nodes.currentNode.parentElement.insertBefore(span, splitNode);
     span.style.borderBottom = '2px solid #45a1ff';
-    span.style.display = 'inline-block';
+    span.style.display = 'inline';
     span.className = `kz-${string.replace(/\s/g, '_')}`;
 
   }
