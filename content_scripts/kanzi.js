1b691dff39520d12e7aa1e4784bfaf353c427137
w3punk <w3punk@pm.me>
2019-02-08 18:34:12 +0300

    Delete custom suffixes feature

--- content_scripts/kanzi.js
+++ content_scripts/kanzi.js
@@ -1,20 +1,9 @@
 browser.runtime.sendMessage({
-  greeting: "Greeting from the content script"
 }).then(message => {
 
   if (message.response) {
     browser.storage.local.get().then(storage => {
-
-      switch (storage.options.suffixes) {
-        case 'english':
-        default:
-          suffixes = '{0,2}(ied|ed|s|es|ies|ing|ings|er|ers|or|ors|y|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ingly)';
-          break;
-        case 'custom':
-          suffixes = storage.options.custom_suffixes || 'Nothing'
-          break;
-      }
-
+      suffixes = '{0,2}(ied|ed|s|es|ies|ing|ings|er|ers|or|ors|y|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ingly)';
       Object.keys(storage.dictionary).forEach(word => wrapWord(word));
       Object.keys(storage.dictionary).forEach(word => createTooltip(word, storage.dictionary[word].meaning, storage.dictionary[word].example));
     });
--- options/options.js
+++ options/options.js
@@ -13,33 +13,3 @@ fontSize.addEventListener('change', function() {
     browser.storage.local.set(o);
   });
 });
-
-//SUFFIXES
-options.then(o => {
-
-  if (o.options.suffixes) {
-    document.querySelector('input[value="' + o.options.suffixes + '"]').checked = true;
-  } else {
-    document.querySelector('input[value="english"]').checked = true;
-  }
-
-  document.querySelector('#custom-suffixes').value = o.options.custom_suffixes;
-
-});
-
-document.querySelector('#suffixes').addEventListener('click', function() {
-  options.then(o => {
-    o.options.suffixes = document.querySelector('input[name="suffixes"]:checked').value;
-    browser.storage.local.set(o);
-  });
-});
-
-document.querySelector('#custom-suffixes').addEventListener('change', function() {
-  options.then(o => {
-    let usersInput = document.querySelector('#custom-suffixes').value;
-    customSuffixes = '{0,2}(' + usersInput.replace(/\s+/g, '|') + ')';
-    o.options.custom_suffixes = customSuffixes;
-    browser.storage.local.set(o);
-  });
-});
-
