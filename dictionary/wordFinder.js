fc62846303bb6027112af7e1b8177540de1e5ac4
w3punk <w3punk@pm.me>
2019-03-04 08:26:33 +0300

    Add word filter

---
+++ dictionary/wordFinder.js
@@ -0,0 +1,22 @@
+let wordFinderInput = document.getElementById('word-finder-input');
+
+wordFinderInput.focus();
+wordFinderInput.addEventListener('keyup', function() {
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("word-finder-input");
+  filter = new RegExp('\\b' + input.value, 'gi');
+  table = document.getElementById("dictionary-table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (filter.test(txtValue)) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+});
