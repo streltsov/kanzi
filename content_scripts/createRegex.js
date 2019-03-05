8c8fc9d70e2b71446646dceb362ffb949c322a85
w3punk <w3punk@pm.me>
2019-03-06 00:59:34 +0300

    Improve createRegex();

--- content_scripts/createRegex.js
+++ content_scripts/createRegex.js
@@ -1,15 +1,6 @@
-let suffixes = '{0,2}(a?i?t?ion|(o|e)r|(i|e)s|i?ed|i?e?ty|i?l?y|i?ness|ing|al|able|ment|ious|ive){1,2}(s)?';
+let suffixes = '{0,2}(ied|ed|es|ies|ing|ings|er|ers|or|ors|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ingly|ary)';
 
 function createRegex(string) {
-  let pattern;
-  if (/\s/.test(string)) {
-    pattern = string.split(' ');
-    pattern.forEach(function(value, i, array) {
-      array[i] = value + suffixes;
-    });
-    pattern = pattern.join(' ');
-  } else {
-    pattern = string + 's?\\b|\\b' + string + suffixes + '\\b';
-  }
-  return new RegExp('\\b' + pattern, 'gi');
+  let pattern = string.split(' ').map(word => '(\\b' + word + '[sy]?\\b|\\b' + word + suffixes + '\\b)').join(' ');
+  return new RegExp(pattern, 'gi');
 }
