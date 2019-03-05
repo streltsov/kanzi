157c7ac71547db32dad30d5b75f7b356284724fc
w3punk <w3punk@pm.me>
2019-03-05 12:24:39 +0300

    Improve english suffixes regex

--- content_scripts/createRegex.js
+++ content_scripts/createRegex.js
@@ -1,4 +1,4 @@
-let suffixes = '{0,2}(ied|ed|s|es|ies|ing|ings|er|ers|or|ors|y|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ingly|ary)';
+let suffixes = '{0,2}(a?i?t?ion|(o|e)r|(i|e)s|i?ed|i?e?ty|i?l?y|i?ness|ing|al|able|ment|ious|ive){1,2}(s)?';
 
 function createRegex(string) {
   let pattern;
