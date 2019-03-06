1d029e5fed353cc950339a0ad30974b95300ed92
w3punk <w3punk@pm.me>
2019-03-06 09:59:49 +0300

    Add flags parameter to createRegex()

--- content_scripts/createRegex.js
+++ content_scripts/createRegex.js
@@ -1,6 +1,8 @@
 let suffixes = '{0,2}(ied|ed|es|ies|ing|ings|er|ers|or|ors|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ingly|ary)';
 
-function createRegex(string) {
+function createRegex(string, flags) {
+  flags = flags || 'gi'
   let pattern = string.split(' ').map(word => '(\\b' + word + '[sy]?\\b|\\b' + word + suffixes + '\\b)').join(' ');
-  return new RegExp(pattern, 'gi');
+  return new RegExp(pattern, flags);
+  
 }
