2e0c71d21d1a61c2f3cedca8eb5f554819e58d0c
w3punk <w3punk@pm.me>
2019-03-06 19:29:38 +0300

    Add 'Add a word' to the popup menu

--- popup/script.js
+++ popup/script.js
@@ -1,3 +1,15 @@
+document.getElementsByClassName("add-word")[0].addEventListener("click", function() {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(tab => {
+    browser.tabs.sendMessage(tab[0].id, {
+      selectedText: ''
+    })
+    window.close();
+  });
+});
+
 document.getElementsByClassName("dictionary")[0].addEventListener("click", function() {
   browser.tabs.create({
     url: '/dictionary/dictionary.html'
