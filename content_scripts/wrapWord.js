9fcb9635c7e85e6d3042f58870d2e798d23e6089
w3punk <w3punk@pm.me>
2019-03-06 10:03:21 +0300

    Improve wrapWord()

--- content_scripts/wrapWord.js
+++ content_scripts/wrapWord.js
@@ -3,25 +3,17 @@ function wrapWord(string) {
 
   let nodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
       acceptNode: function(node) {
-        if (re.test(node.textContent) && !/^(STYLE|SCRIPT)$/i.test(node.parentNode.nodeName)) //Test!!!
+        if (re.test(node.textContent) && !/^(STYLE|SCRIPT)$/i.test(node.parentNode.nodeName))
           return NodeFilter.FILTER_ACCEPT;
       }
     },
     false);
 
-  let wordAmount = string.split(' ').length;
-
   while (nodes.nextNode()) {
 
-    let wordStart = nodes.currentNode.textContent.search(re);
-    let splitNode = nodes.currentNode.splitText(wordStart);
-    let stringOnPage = splitNode.textContent.split(' ', wordAmount);
-
-    if (XRegExp('\\PL').test(stringOnPage[wordAmount - 1])) {
-      stringOnPage[wordAmount - 1] = stringOnPage[wordAmount - 1].replace(XRegExp('\\PL.*', 'g'), '');
-    }
-
-    stringOnPage = stringOnPage.join(' ');
+    let ex = createRegex(string, 'i').exec(nodes.currentNode.textContent);
+    let splitNode = nodes.currentNode.splitText(ex.index);
+    let stringOnPage = ex[0];
     nodes.currentNode.nextSibling.data = nodes.currentNode.nextSibling.data.slice(stringOnPage.length);
 
     let span = document.createElement('span');
