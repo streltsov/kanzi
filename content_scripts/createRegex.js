c90818e7f5932e62c00fd83318b3d84e06456483
w3punk <w3punk@pm.me>
2019-03-07 10:19:10 +0300

    Improve regex

--- content_scripts/createRegex.js
+++ content_scripts/createRegex.js
@@ -1,8 +1,8 @@
-let suffixes = '{0,2}(ied|ed|es|ies|ing|ings|er|ers|or|ors|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ingly|ary)';
+let suffixes = '{0,2}(ied|ies|ing|ings|ingly|er|ers|or|ors|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ary)';
 
 function createRegex(string, flags) {
   flags = flags || 'gi'
-  let pattern = string.split(' ').map(word => '(\\b' + word + '[sy]?\\b|\\b' + word + suffixes + '\\b)').join(' ');
+  let pattern = string.split(' ').map(word => '(\\b' + word + '([sdy]|(ed))?\\b|\\b' + word + suffixes + '\\b)').join(' ');
   return new RegExp(pattern, flags);
   
 }
