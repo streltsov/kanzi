468d7d2e9b28248fe4ac3edc5b259621938e15fd
w3punk <w3punk@pm.me>
2019-03-20 10:08:43 +0300

    Style popup firefox alike

--- content_scripts/kanzi.js
+++ content_scripts/kanzi.js
@@ -6,6 +6,7 @@ browser.storage.local.get().then(storage => {
     wrapWord(word);
     createTooltip(word, storage.dictionary[word].meaning, storage.dictionary[word].example)
   });
+  getStatistics(storage.dictionary);
 });
 
 browser.runtime.onMessage.addListener(request => createModal(request.selectedText));
@@ -14,5 +15,25 @@ function unwrapWord(word) {
   word = word.trim().toLowerCase();
   document.querySelectorAll(`.kz-${word.replace(/\s/g, '_')}`).forEach(wrapper => {
     wrapper.outerHTML = wrapper.outerHTML.replace(wrapper.outerHTML, wrapper.innerText);
+  });
+}
+
+function getStatistics(dict) {
+  const spans = [].slice.call(document.querySelectorAll('.kz-word'));
+  const words = spans.map(span => span.className.slice(11).replace(/_/g, ' '));
+  const uniq = Array.from(new Set(words));
+
+  uniq.forEach(word => {
+    browser.storage.local.get('dictionary').then(d => {
+      if (d.dictionary[word].frequency == undefined) {
+        d.dictionary[word].frequency = 1;
+        browser.storage.local.set(d);
+        console.log(word + ' ' + d.dictionary[word].frequency);
+      } else {
+        d.dictionary[word].frequency += 1;
+        browser.storage.local.set(d);
+        console.log(word + ' ' + d.dictionary[word].frequency);
+      }
+    });
   });
 }
