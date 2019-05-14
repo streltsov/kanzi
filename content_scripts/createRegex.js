a94c26ec39bb9f7ac9f4b5cbb35be3cd3fae3d28
w3punk <w3punk@pm.me>
2019-05-14 10:12:59 +0300

    Fix typo in default flags
    Delete space in ' gi'

--- content_scripts/createRegex.js
+++ content_scripts/createRegex.js
@@ -1,7 +1,7 @@
 const suffixes =
   '{0,2}(ied|ies|ing|ings|ingly|er|ers|or|ors|ly|ily|ty|ity|ety|ive|al|ally|able|ion|ions|ious|tion|ation|ition|ication|iness|ness|ment|ure|ish|ary)';
 
-const createRegex = (string, flags = ' gi') => {
+const createRegex = (string, flags = 'gi') => {
   const pattern = string
     .split(' ')
     .map(
