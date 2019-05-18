317ce117bafc2cc4a414987483fc2371a745729f
w3punk <w3punk@pm.me>
2019-05-18 16:37:46 +0300

    Refactor

--- content_scripts/createModal.js
+++ content_scripts/createModal.js
@@ -1,128 +1,93 @@
-const createModal = (word, meaning = '', example = '') => {
-  word = word.trim().toLowerCase();
-
-  const modal = document.createElement('div');
-  modal.className = 'kz-overlay';
-
-  const modalContent = document.createElement('div');
-  modalContent.className = 'kz-modal-content';
-
-  const wordField = document.createElement('input');
-  wordField.value = word;
-  wordField.className = 'kz-word-field';
-
-  const meaningLabel = document.createElement('label');
-  meaningLabel.className = 'kz-meaning-label';
-  meaningLabel.setAttribute('for', 'kz-meaning-field');
-  const meaningLabelText = document.createTextNode(
-    browser.i18n.getMessage('modalMeaningLabel'),
-  );
-  meaningLabel.appendChild(meaningLabelText);
-
-  const meaningField = document.createElement('textarea');
-  meaningField.placeholder = browser.i18n.getMessage('modalMeaningPlaceholder');
-  meaningField.maxLength = '5000';
-  meaningField.className = 'kz-meaning-field';
-  meaningField.id = 'kz-meaning-field';
-  if (meaningField) meaningField.value = meaning;
-
-  const exampleLabel = document.createElement('label');
-  exampleLabel.className = 'kz-example-label';
-  exampleLabel.setAttribute('for', 'kz-example-field');
-  const exampleLabelText = document.createTextNode(
-    browser.i18n.getMessage('modalExampleLabel'),
-  );
-  exampleLabel.appendChild(exampleLabelText);
-
-  const exampleField = document.createElement('textarea');
-  exampleField.placeholder = browser.i18n.getMessage('modalExamplePlaceholder');
-  exampleField.maxLength = '5000';
-  exampleField.className = 'kz-example-field';
-  exampleField.id = 'kz-example-field';
-  if (exampleField) exampleField.value = example;
-
-  const closeButton = document.createElement('button');
+const createInput = value => {
+  const input = document.createElement('input');
+  input.value = value;
+  return input;
+};
+const createLabel = text => {
+  const label = document.createElement('label');
+  label.className = 'kz-label';
+  label.textContent = text;
+  return label;
+};
+const createButton = text => {
+  const button = document.createElement('button');
+  button.textContent = text;
+  return button;
+};
+const doneButton = () => {
+  const doneButton = createButton('Done');
+  doneButton.className = 'kz-done-button';
+  doneButton.addEventListener('click', e => doneAction(e.target));
+  return doneButton;
+};
+const closeButton = () => {
+  const closeButton = createButton('X');
   closeButton.className = 'kz-close-button';
-  closeButton.onclick = () => modal.remove();
+  closeButton.addEventListener('click', closeModal);
+  return closeButton;
+};
+const extraField = (labelText, inputText) => {
+  const label = createLabel(labelText);
+  const input = createInput(inputText);
+  input.className = 'kz-extra-field';
+  label.appendChild(input);
+  return label;
+};
+const closeModal = () => document.querySelector('.kz-modal-root').remove();
+function doneAction(el) {
+  const word = el.parentNode
+    .querySelectorAll('input')[0]
+    .value.trim()
+    .toLowerCase();
+  const meaning = el.parentNode.querySelectorAll('input')[1].value.trim();
+  const example = el.parentNode.querySelectorAll('input')[2].value.trim();
 
-  const closeButtonIcon = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'svg',
-  );
-  closeButtonIcon.setAttribute('width', '24');
-  closeButtonIcon.setAttribute('height', '24');
-  closeButtonIcon.setAttribute('viewBox', '0 0 24 24');
-
-  const closeButtonIconPath = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'path',
-  );
-  closeButtonIconPath.setAttribute('fill', 'rgba(12, 12, 13, .8)');
-  closeButtonIconPath.setAttribute(
-    'd',
-    'M5.293 6.707a1 1 0 1 1 1.414-1.414L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12 5.293 6.707z',
-  );
-  closeButtonIcon.appendChild(closeButtonIconPath);
-  closeButton.appendChild(closeButtonIcon);
-
-  const addButton = document.createElement('button');
-  addButton.className = 'kz-add-button';
-
-  const addButtonText = document.createTextNode(
-    browser.i18n.getMessage('modalDone'),
-  );
-  addButton.appendChild(addButtonText);
-  addButton.addEventListener('click', () => {
-    if (wordField.value) {
-      word = wordField.value.trim().toLowerCase();
-      meaning = meaningField.value.trim();
-      example = exampleField.value.trim();
-      unwrapWord(word);
-      addWordToDictionary(word, meaning, example);
-      modal.remove();
-      wrapWord(word);
-      createTooltip(word, meaning, example);
-    }
-  });
-
-  modalContent.appendChild(wordField);
-  modalContent.appendChild(meaningLabel);
-  modalContent.appendChild(meaningField);
-  modalContent.appendChild(exampleLabel);
-  modalContent.appendChild(exampleField);
-  modalContent.appendChild(addButton);
-  modalContent.appendChild(closeButton);
-  modal.appendChild(modalContent);
-
-  const shadowDomRoot = document.createElement('div');
-  shadowDomRoot.className = 'shadow-dom-root-modal';
-  const shadow = shadowDomRoot.attachShadow({
-    mode: 'open',
-  });
+  unwrapWord(word);
+  addWordToDictionary(word, meaning, example);
+  closeModal();
+  wrapWord(word);
+  createTooltip(word, meaning, example);
+}
+const modal = (word, meaning, example) => {
+  const root = document.createElement('div');
+  root.className = 'kz-modal-root';
+  const shadow = root.attachShadow({mode: 'open'});
   const style = document.createElement('style');
-
-  style.textContent = `.kz-overlay{position:fixed;z-index:999999;padding-top:150px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:rgba(12, 12, 13, 0.6)}.kz-modal-content{display:flex;flex-direction:column;background-color:#f9f9fa;border:2px solid rgba(12, 12, 13, 0.2);border-radius:2px;max-width:40%;margin:0 auto;padding:0 16px;position:relative}.kz-word-field{background:#f9f9fa;border:none;color:#ff0039;font-family:'Cormorant Garamond', serif;font-weight:300;font-size:36px;padding:0;margin-top:8px;text-align:center}.kz-example-label,.kz-meaning-label{color:#0c0c0d;margin-bottom:4px;font-size:0.833rem}.kz-example-label{margin-top:8px}.kz-example-field,.kz-meaning-field{padding:8px;width:auto;height:32px;border-radius:2px;resize:none;border:1px solid rgba(12, 12, 13, 0.2);color:#0c0c0d;background-color:white;box-sizing:content-box}.kz-example-field::placeholder,.kz-meaning-field::placeholder{color:#737373}.kz-example-field:focus,.kz-meaning-field:focus{border:1px solid #0a84ff;box-shadow:0 0 0 1px #0a84ff, 0 0 0 4px rgba(10, 132, 255, 0.3)}.kz-example-field:hover,.kz-meaning-field:hover{border:1px solid rgba(12, 12, 13, 0.3)}.kz-add-button{align-self:flex-end;background-color:#0060df;border:none;color:#fff;border-radius:2px;font-size:13px;font-weight:400;height:32px;min-width:132px;padding:0 8px;margin:8px 0 12px}.kz-add-button:hover{background-color:#003eaa}.kz-add-button:active{background-color:#002275}.kz-add-button:focus{box-shadow:0 0 0 1px #0a84ff inset, 0 0 0 1px #0a84ff, 0 0 0 4px rgba(10, 132, 255, 0.3)}.kz-close-button{background-color:transparent;border:none;border-radius:2px;position:absolute;right:8px;top:8px;padding:4px;margin:0;height:32px;width:32px}svg{display:block}.kz-close-button:hover{background-color:rgba(12, 12, 13, 0.1)}.kz-close-button:active{background-color:rgba(12, 12, 13, 0.2)}.kz-close-button:focus{box-shadow:0 0 0 1px #0a84ff inset, 0 0 0 1px #0a84ff, 0 0 0 4px rgba(10, 132, 255, 0.3)}.kz-close-button,.kz-word-field{z-index:2}`;
-
-  const script = document.createElement('script');
-  script.textContent = `
-  window.addEventListener("keydown", e => {
-    if (e.keyCode === 27 && !!document.querySelector('.shadow-dom-root-modal')) {
-      e.preventDefault();
-      document.querySelector(".shadow-dom-root-modal").remove();
-    }
-  });
-
-  window.addEventListener("keydown", e => {
-    if (e.keyCode === 13 && !!document.querySelector('.shadow-dom-root-modal')) {
-      e.preventDefault();
-      document.querySelector('.shadow-dom-root-modal').shadowRoot.querySelector('.kz-add-button').click();
-    }
-  });`;
-
+  style.textContent = `.kz-overlay{position:fixed;z-index:999999;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:rgba(12,12,13,.6)}.kz-modal{display:flex;flex-direction:column;background-color:#f9f9fa;border:2px solid rgba(12,12,13,.2);border-radius:2px;width:540px;max-width:80%;margin:100px auto 0 auto;padding:0 16px;position:relative}.kz-word-field{background:#f9f9fa;border:none;color:#ff0039;font-family:'Cormorant Garamond',serif;font-weight:300;font-size:36px;padding:0;margin-top:8px;text-align:center}.kz-label{color:#0c0c0d;margin-bottom:4px;font-size:.833rem;display:flex;flex-direction:column}.kz-extra-field{padding:8px;width:auto;height:32px;border-radius:2px;resize:none;border:1px solid rgba(12,12,13,.2);color:#0c0c0d;background-color:#fff;box-sizing:content-box}.kz-extra-field::placeholder{color:#737373}.kz-extra-field:focus{border:1px solid #0a84ff;box-shadow:0 0 0 1px #0a84ff,0 0 0 4px rgba(10,132,255,.3)}.kz-extra-field:hover{border:1px solid rgba(12,12,13,.3)}.kz-done-button{align-self:flex-end;background-color:#0060df;border:none;color:#fff;border-radius:2px;font-size:13px;font-weight:400;height:32px;min-width:132px;padding:0 8px;margin:8px 0 12px}.kz-done-button:hover{background-color:#003eaa}.kz-done-button:active{background-color:#002275}.kz-done-button:focus{box-shadow:0 0 0 1px #0a84ff inset,0 0 0 1px #0a84ff,0 0 0 4px rgba(10,132,255,.3)}.kz-done-button:disabled{opacity:.4}.kz-close-button{color:#333;background-color:transparent;border:none;border-radius:2px;position:absolute;right:8px;top:8px;padding:4px;margin:0;height:32px;width:32px}.kz-close-button:hover{background-color:rgba(12,12,13,.1)}.kz-close-button:active{background-color:rgba(12,12,13,.2)}.kz-close-button:focus{box-shadow:0 0 0 1px #0a84ff inset,0 0 0 1px #0a84ff,0 0 0 4px rgba(10,132,255,.3)}`;
   shadow.appendChild(style);
-  shadow.appendChild(script);
-  shadow.appendChild(modal);
-  document.body.appendChild(shadowDomRoot);
+  const overlay = document.createElement('div');
+  overlay.className = 'kz-overlay';
+  shadow.appendChild(overlay);
+  const modal = document.createElement('div');
+  modal.className = 'kz-modal';
+  overlay.appendChild(modal);
+  modal.appendChild(closeButton());
+  const wordField = createInput(word);
+  wordField.className = 'kz-word-field';
+  wordField.addEventListener(
+    'input',
+    e =>
+      (wordField.parentNode.querySelector('.kz-done-button').disabled = !e
+        .target.value),
+  );
+  modal.appendChild(wordField);
+  modal.appendChild(extraField('Meaning', meaning));
+  modal.appendChild(extraField('Example', example));
+  modal.appendChild(doneButton());
+  shadow.addEventListener('keydown', e => {
+    e.keyCode === 27 && document.querySelector('.kz-modal-root').remove(),
+      e.keyCode === 13 &&
+        e.target.nodeName == 'INPUT' &&
+        doneAction(e.target.parentNode);
+  });
+  return root;
+};
 
+const createModal = (word, meaning = '', example = '') => {
+  const body = document.querySelector('body');
+  body.appendChild(modal(word, (meaning = ''), (example = '')));
+  const wordField = document
+    .querySelector('.kz-modal-root')
+    .shadowRoot.querySelector('.kz-word-field');
   wordField.focus();
 };
