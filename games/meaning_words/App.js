467582fb9483bb4a657104439755cc78c5b24917
streltsov <w3punk@pm.me>
2019-06-13 11:39:05 +0300

    Get random answers

--- games/meaning_words/App.js
+++ games/meaning_words/App.js
@@ -1,5 +1,7 @@
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -44,13 +46,17 @@ var App = function (_React$Component) {
         _this.setState({ words: _this.sort(dictionary) });
         console.table(_this.state.words);
       });
+    }, _this.getAnswers = function (array) {
+      return [array[0]].concat(_toConsumableArray(Array.from({ length: 3 }, function () {
+        return array[Math.floor(Math.random() * array.length)];
+      })));
     }, _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(App, [{
     key: 'render',
     value: function render() {
-      return this.state.words ? React.createElement(Answers, { answers: this.state.words.slice(0, 4) }) : React.createElement(
+      return this.state.words ? React.createElement(Answers, { answers: this.getAnswers(this.state.words) }) : React.createElement(
         'div',
         null,
         'Loading...'
