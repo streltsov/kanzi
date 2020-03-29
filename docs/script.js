6e7599b72f7abce14336261bebb617ecb4d6c6e7
Herman Streltsov <mail@streltsov.org>
2020-03-29 19:11:32 +0300

    Remove all files

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-const notAvailText = document.createElement('i');
-notAvailText.textContent = 'Available for Firefox only';
-notAvailText.className = 'not-firefox';
-const addButton = document.querySelector('.add-button');
-
-if (!/firefox/.test(navigator.userAgent.toLowerCase())) {
-  addButton.parentNode.insertBefore(notAvailText, addButton.nextSibling);
-  addButton.style.display = 'none';
-}
